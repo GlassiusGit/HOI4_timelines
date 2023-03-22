c1f29b9fa5a6b3dcc3e0a9d72401687565fe8d5b
glassius <glassius_89@o2.pl>
2023-03-22 04:09:17 +0100

    Utilizing new csv file data in pands DataFrame

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 import plotly.graph_objects as go
 import datetime as dt
+import pandas as pd
+from math import isnan
 
 
 fig = go.Figure()
-
-'''
-# Create scatter trace of text labels
-fig.add_trace(go.Scatter(
-    x=[1.5, 3],
-    y=[2.5, 2.5],
-    text=["Rectangle reference to the plot",
-          "Rectangle reference to the axes"],
-    mode="text",
-))
-'''
 
 # Set axes properties
 fig.update_xaxes(range=[dt.date(1936,1,1), dt.date(1940,1,1)], type="date")
@@ -27,69 +18,31 @@ focus_colors= {
 }
 
 
-source = [
-    dict(Resource="POL_focus",  Task="Complete the April Constitution",        Duration=70,    Type="politics"),
-    dict(Resource="POL_focus",  Task="The Four Year Plan",                     Duration=35,    Type="economy"),
-    dict(Resource="POL_focus",  Task="Central Region Strategy",                Duration=35,    Type="economy"),
-    dict(Resource="POL_focus",  Task="The New Polish Industry",                Duration=35,    Type="economy"),
-    dict(Resource="POL_focus",  Task="Fill the Railway Gaps",                  Duration=70,    Type="economy"),
-    dict(Resource="POL_focus",  Task="Polish Militarism",                      Duration=35,    Type="politics"),
-    dict(Resource="POL_focus",  Task="Consolidate the Sanation Government",    Duration=35,    Type="politics"),
-    dict(Resource="POL_focus",  Task="Agrarian Reform",                        Duration=70,    Type="economy"),
-    dict(Resource="POL_focus",  Task="The Castle",                             Duration=35,    Type="politics"),
-    dict(Resource="POL_focus",  Task="Central Defence of Poland",              Duration=70,    Type="economy"),
-    dict(Resource="POL_focus",  Task="The Sanation Right",                     Duration=35,    Type="politics"),
-    dict(Resource="POL_focus",  Task="Eliminate the Socialist Parties",        Duration=35,    Type="politics"),
-    dict(Resource="POL_focus",  Task="Develop Upper Silesia",                  Duration=35,    Type="economy"),
-    dict(Resource="POL_focus",  Task="Polish School of Mathematics",           Duration=70,    Type="economy"),
-    dict(Resource="POL_focus",  Task="Dissolve the Sejm",                      Duration=35,    Type="politics"),
-    dict(Resource="POL_focus",  Task="Support Right Wing Paramilitarism",      Duration=35,    Type="politics"),
-    dict(Resource="POL_focus",  Task="Department of Home Defence",             Duration=35,    Type="politics"),
-    dict(Resource="POL_focus",  Task="Develop Polish Shipbuilding",            Duration=70,    Type="marine"),
-    dict(Resource="POL_focus",  Task="Attract Poles to Gdynia",                Duration=70,    Type="marine"),
-    dict(Resource="POL_focus",  Task="Study British Ship Designs",             Duration=70,    Type="marine"),
-    dict(Resource="POL_focus",  Task="Expand the Gdynia Seaport",              Duration=70,    Type="marine"),
-    dict(Resource="POL_focus",  Task="Second Man of the State",                Duration=70,    Type="politics"),
-    dict(Resource="POL_focus",  Task="Invest in the Old Polish Region",        Duration=35,    Type="economy"),
-    dict(Resource="POL_focus",  Task="Modernise the Congressional Factories",  Duration=70,    Type="economy"),
-    dict(Resource="POL_focus",  Task="Warsaw Main Railway Station",            Duration=70,    Type="economy"),
-    dict(Resource="POL_focus",  Task="Invest in Eastern Poland",               Duration=70,    Type="economy"),
-    dict(Resource="POL_focus",  Task="National Defense Fund",                  Duration=70,    Type="economy"),
-    dict(Resource="real",       Task="April Constitution",                          Date='1935-04-23', Parent=["Complete the April Constitution"]),
-    dict(Resource="real",       Task="New statute of Polish Mathematical Society",  Date='1936-03-14', Parent=["Polish School of Mathematics"]),
-    dict(Resource="real",       Task="Foreign exchange controls",                   Date='1936-04-26', Parent=[]),
-    dict(Resource="real",       Task="Składkowski's government",                    Date='1936-05-16', Parent=["Consolidate the Sanation Government"]),
-    dict(Resource="real",       Task="Four Year Plan ",                             Date='1936-07-01', Parent=["The Four Year Plan"]),
-    dict(Resource="real",       Task="A note to ministers and voivodes",            Date='1936-07-13', Parent=["Second Man of the State"]),
-    dict(Resource="real",       Task="Marshal of Poland",                           Date='1936-11-10', Parent=["The Sanation Right"]),
-    dict(Resource="real",       Task="Mościcki denies Składkowskis resignation",    Date='1937-06-23', Parent=["The Castle"]),
-    dict(Resource="real",       Task="Kwiatkowski's speech",                        Date='1937-07-05', Parent=["Central Region Strategy"]),
-    dict(Resource="real",       Task="Mobilization plan W",                         Date='1938-05-01', Parent=["Polish Militarism"]),
-    dict(Resource="real",       Task="Election 1938",                               Date='1938-11-06', Parent=["Dissolve the Sejm"]),
-    
-    
-]
+source = pd.read_csv(r"C:\Users\Patryk\Desktop\Nowy folder\NSB_original.csv", encoding="utf-8")
 
-previous_finish = dt.date(1936,1,1)
+previous_finish = dt.date(1936,1,1).isoformat()
 day = dt.timedelta(days=1)
 
 def get_original(key, value, source):
-    for shape in source:
+    for index, shape in source.iterrows():
         if shape[key] == value:
             return shape
 
-for shape in source:
+for index, shape in source.iterrows():
     if shape["Resource"] == "POL_focus":
-        shape["Start"] = previous_finish
-        shape["Finish"] = (shape["Start"] + shape["Duration"] * day)
-        previous_finish = shape["Finish"]
+        source.at[index, "Start"] = previous_finish
+        start = dt.date.fromisoformat(source.at[index, "Start"])
+        duration = shape["Duration"] *  day
+        finish = start + duration
+        source.at[index, "Finish"] = finish.isoformat()
+        previous_finish = source.at[index, "Finish"]
         
         fig.add_shape(type="rect",
                       xref="x",
                       yref="y",
-                      x0=shape["Start"].isoformat(),
+                      x0=source.at[index, "Start"],
                       y0=2,
-                      x1=shape["Finish"].isoformat(),
+                      x1=source.at[index, "Finish"],
                       y1=4.5,
                       line=dict(
                           color=focus_colors[shape["Type"]][0],
@@ -97,8 +50,9 @@ for shape in source:
                           ),
                       fillcolor=focus_colors[shape["Type"]][1],
                       )
+
         fig.add_annotation(
-            x=(shape["Start"] + (shape["Finish"]-shape["Start"])/2).isoformat(),
+            x=(start + (finish - start)/2).isoformat(),
             y=2,
             text=shape["Task"],
             showarrow=False,
@@ -108,7 +62,8 @@ for shape in source:
             yanchor="bottom",
             )
 
-for shape in source:
+
+for index, shape in source.iterrows():
     if shape["Resource"] == "real":
         end_point= shape["Date"]
         fig.add_annotation(
@@ -119,10 +74,12 @@ for shape in source:
             textangle=-90,
             yanchor="top",
             )
-        for parent in shape["Parent"]:
+        parent = shape["Parent"];
+        if type(parent) is str:
             original_task = get_original("Task", parent, source)
-            
-            start_point = original_task["Finish"].isoformat()
+            print(parent)
+            print(original_task)
+            start_point = dt.date.fromisoformat(original_task["Finish"])
             end_point= shape["Date"]
             colors = focus_colors[original_task["Type"]]
 
@@ -136,5 +93,5 @@ for shape in source:
                               width=4
                               )
                           )
-            
+     
 fig.show()
