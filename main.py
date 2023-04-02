4b94ad6a2aa5a2a8b5074654c9ab5bd09b969580
glassius <glassius_89@o2.pl>
2023-04-03 01:04:59 +0200

    Rewrite main.py to utilize new csv files

--- main.py
+++ main.py
@@ -3,12 +3,25 @@ import datetime as dt
 import pandas as pd
 from math import isnan
 
+def get_original(key, value, source):
+    for index, shape in source.iterrows():
+        if shape[key] == value:
+            return shape
+
+
+def populate_dates(starting_date, df):
+    current_date = dt.date.fromisoformat(starting_date)
+    for index, focus in df.iterrows():
+        df.at[index, "Start"] = current_date
+        delta = df.at[index, "Duration"] * dt.timedelta(days=1)
+        current_date += delta
+        df.at[index, "End"] = current_date
+
 
 fig = go.Figure()
-
-# Set axes properties
 fig.update_xaxes(range=[dt.date(1936,1,1), dt.date(1940,1,1)], type="date")
 fig.update_yaxes(range=[-1, 4.5])
+
 
 focus_colors= {
     "politics": ("Gray", "Silver"),
@@ -18,70 +31,59 @@ focus_colors= {
 }
 
 
-source = pd.read_csv(r"C:\Users\Patryk\Desktop\Nowy folder\NSB_original.csv", encoding="utf-8")
-
-previous_finish = dt.date(1936,1,1).isoformat()
-day = dt.timedelta(days=1)
-
-def get_original(key, value, source):
-    for index, shape in source.iterrows():
-        if shape[key] == value:
-            return shape
-
-for index, shape in source.iterrows():
-    if shape["Resource"] == "POL_focus":
-        source.at[index, "Start"] = previous_finish
-        start = dt.date.fromisoformat(source.at[index, "Start"])
-        duration = shape["Duration"] *  day
-        finish = start + duration
-        source.at[index, "Finish"] = finish.isoformat()
-        previous_finish = source.at[index, "Finish"]
-        
-        fig.add_shape(type="rect",
-                      xref="x",
-                      yref="y",
-                      x0=source.at[index, "Start"],
-                      y0=2,
-                      x1=source.at[index, "Finish"],
-                      y1=4.5,
-                      line=dict(
-                          color=focus_colors[shape["Type"]][0],
-                          width=3,
-                          ),
-                      fillcolor=focus_colors[shape["Type"]][1],
-                      )
-
-        fig.add_annotation(
-            x=(start + (finish - start)/2).isoformat(),
-            y=2,
-            text=shape["Task"],
-            showarrow=False,
-            arrowhead=1,
-            yshift=0,
-            textangle=-90,
-            yanchor="bottom",
-            )
+# NSB data
+NSB = pd.read_csv(r"C:\Users\Patryk\Desktop\Nowy folder\NSB_original.csv", encoding="utf-8")
+populate_dates('1936-01-01', NSB)
 
 
-for index, shape in source.iterrows():
-    if shape["Resource"] == "real":
-        end_point= shape["Date"]
-        fig.add_annotation(
-            x=end_point,
-            y=1,
-            text=shape["Task"],
-            showarrow=False,
-            textangle=-90,
-            yanchor="top",
-            )
-        parent = shape["Parent"];
-        if type(parent) is str:
-            original_task = get_original("Task", parent, source)
-            print(parent)
-            print(original_task)
-            start_point = dt.date.fromisoformat(original_task["Finish"])
-            end_point= shape["Date"]
-            colors = focus_colors[original_task["Type"]]
+for index, focus in NSB.iterrows():
+    start = NSB.at[index, "Start"]
+    end = NSB.at[index, "End"]
+    fig.add_shape(type="rect",
+                  xref="x",
+                  yref="y",
+                  x0=start,
+                  y0=2,
+                  x1=end,
+                  y1=4.5,
+                  line=dict(
+                      color=focus_colors[focus["Type"]][0],
+                      width=3,
+                      ),
+                  fillcolor=focus_colors[focus["Type"]][1],
+                  )
+
+    fig.add_annotation(
+        x=(start + (end - start)/2).isoformat(),
+        y=2,
+        text=focus["Focus"],
+        showarrow=False,
+        arrowhead=1,
+        yshift=0,
+        textangle=-90,
+        yanchor="bottom",
+        )
+
+# real events
+reals = pd.read_csv(r"C:\Users\Patryk\Desktop\Nowy folder\real_events.csv")
+
+for index, event in reals.iterrows():
+    end_point= event["Date"]
+    fig.add_annotation(
+        x=end_point,
+        y=1,
+        text=event["Event"],
+        showarrow=False,
+        textangle=-90,
+        yanchor="top",
+        )
+    parents = event["Focuses"];
+    if type(parents) is str:
+        for parent in parents.split(';'):
+            original_task = get_original("Focus", parent, NSB)
+            start_point = original_task["End"]
+            end_point= event["Date"]
+            colors = event["Type"]
 
             fig.add_shape(type="line",
                           x0=start_point,
@@ -89,7 +91,7 @@ for index, shape in source.iterrows():
                           x1=end_point,
                           y1=1,
                           line=dict(
-                              color=colors[0],
+                              color=focus_colors[event["Type"]][0],
                               width=4
                               )
                           )
